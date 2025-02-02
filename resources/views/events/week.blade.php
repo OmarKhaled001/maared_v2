0b289565f8a7a365d183c9cc28418b7b0d4dc072
Omar Khaled <omarkhaled102002@gmail.com>
2025-02-02 21:09:12 +0200

    all

--- resources/views/events/week.blade.php
+++ resources/views/events/week.blade.php
@@ -1,10 +1,11 @@
-
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="vertical" data-sidebar="dark" data-sidebar-size="lg" data-preloader="disable" data-theme="default" data-bs-theme="light" data-topbar="light">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="vertical" data-sidebar="dark"
+    data-sidebar-size="lg" data-preloader="disable" data-theme="default" data-bs-theme="light" data-topbar="light">
 <title>الاحداث</title>
 <meta content="Minimal Admin & dashboard Template" name="description">
-    @include('main.meta')
+@include('main.meta')
 </head>
+
 <body>
     <div id="layout-wrapper">
         @include('main.topbar')
@@ -14,58 +15,86 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-lg-12">
-                          <div class="card">
-                            <div class="card-header">
-                                <h4 class="card-title">الاحداث</h4>
-                            </div>
-                            <div class="card-body">
-                                <div >
-                                    <table id="buttons-datatables" class="table buttons nowrap align-middle text-center" style="width:100%">
-                                      <thead>
-                                        <tr>
-                                            <th>التاريخ</th>
-                                            <th>المشاركة</th>
-                                            <th>الصورة</th>
-                                            <th>الأسماء</th>
-                                            <th>الأرقام</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @if (count($events)>0)
-                                            @foreach ($events as $event)
-                                            <tr>
-                                                <td rowspan="{{count($event->volunteers)+1}}">{{$event->date}}</td>
-                                                
-                                                
-                                                <td rowspan="{{count($event->volunteers)+1}}">{{$event->type}}</td>
-                                                <td rowspan="{{count($event->volunteers)+1}}">
-                                                    @if($event->getMedia('event_screns')->first() !== null)
-                                                   <img  alt="profile" height="75" width="75" class="object-fit-cover text-center my-3" src="
-                                                   
-                                                   
-                                                       {{$event->getMedia('event_screns')->first()->getUrl()}}
-                                                       ">
-                                                       @endif
-                                                </td>
-                                                @foreach($event->volunteers as $volunteer)
-                                                <tr>
-                                                    <td>{{$volunteer->name}}</td>
-                                                    <td>{{$volunteer->phone}}</td>
-                                                </tr>
-                                                @endforeach
-                                                
-                                            </tr>
-                                            
-                                            @endforeach
-                                            @endif
-                                    </tbody>
-                                        
-                                    </table>
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">الاحداث</h4>
                                 </div>
-                            </div><!-- end card-body -->
-                          </div><!-- end card -->
+                                @if (Request::url() == route('events.week5'))
+                                    <!-- Base Example -->
+                                    <div class="accordion" id="default-accordion-example">
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="headingOne">
+                                                <button class="accordion-button" type="button"
+                                                    data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                                                    aria-expanded="true" aria-controls="collapseOne">
+                                                    فلتر
+                                                </button>
+                                            </h2>
+                                            <div id="collapseOne" class="accordion-collapse collapse show"
+                                                aria-labelledby="headingOne"
+                                                data-bs-parent="#default-accordion-example">
+                                                <div class="accordion-body">
+                                                    Although you probably won’t get into any legal trouble if you do it
+                                                    just once, why risk it? If you made your subscribers a promise, you
+                                                    should honor that. If not, you run the risk of a drastic increase in
+                                                    opt outs, which will only hurt you in the long run.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+                                <div class="card-body">
+                                    <div>
+                                        <table id="buttons-datatables"
+                                            class="table buttons nowrap align-middle text-center" style="width:100%">
+                                            <thead>
+                                                <tr>
+                                                    <th>التاريخ</th>
+                                                    <th>المشاركة</th>
+                                                    <th>الصورة</th>
+                                                    <th>الأسماء</th>
+                                                    <th>الأرقام</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @if (count($events) > 0)
+                                                    @foreach ($events as $event)
+                                                        <tr>
+                                                            <td rowspan="{{ count($event->volunteers) + 1 }}">
+                                                                {{ $event->date }}</td>
+
+
+                                                            <td rowspan="{{ count($event->volunteers) + 1 }}">
+                                                                {{ $event->type }}</td>
+                                                            <td rowspan="{{ count($event->volunteers) + 1 }}">
+                                                                @if ($event->getMedia('event_screns')->first() !== null)
+                                                                    <img alt="profile" height="75" width="75"
+                                                                        class="object-fit-cover text-center my-3"
+                                                                        src="
+                                                   
+                                                   
+                                                       {{ $event->getMedia('event_screns')->first()->getUrl() }}
+                                                       ">
+                                                                @endif
+                                                            </td>
+                                                            @foreach ($event->volunteers as $volunteer)
+                                                        <tr>
+                                                            <td>{{ $volunteer->name }}</td>
+                                                            <td>{{ $volunteer->phone }}</td>
+                                                        </tr>
+                                                    @endforeach
+
+                                                    </tr>
+                                                @endforeach
+                                                @endif
+                                            </tbody>
+
+                                        </table>
+                                    </div>
+                                </div><!-- end card-body -->
+                            </div><!-- end card -->
                         </div>
-                    
+
                     </div>
                 </div>
             </div>
@@ -74,4 +103,5 @@
     </div>
     @include('main.scripts')
 </body>
+
 </html>
